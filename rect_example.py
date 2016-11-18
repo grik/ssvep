81f28202975f6c3eadb40d339f33e917a9306194
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2016-11-18 18:09:25 +0100

    rect example

---
+++ rect_example.py
@@ -0,0 +1,14 @@
+from psychopy import visual, core  # import some libraries from PsychoPy
+
+#create a window
+mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
+
+#create some stimuli
+rect = visual.Rect(win=mywin, fillColor='red', lineColor='red', size=10)
+
+#draw the stimuli and update the window
+rect.draw()
+mywin.update()
+
+#pause, so you get a chance to see it!
+core.wait(5.0)
