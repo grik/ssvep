9d6b0951909f07af8746c63247310eb98a781c26
mikbuch <mikolaj.buchwald@gmail.com>
2016-11-30 01:48:28 +0100

    better flash for 2 rectangles

--- flash_2rectangles.py
+++ flash_2rectangles.py
@@ -10,47 +10,54 @@ import numpy as np
 #
 
 # Create a window.
+# For configuring and debugging the code turn off full screen.
+fullscr = False
 win = visual.Window(
     [1200,1000],
     monitor="testMonitor",
-    units="deg"
+    units="deg",
+    fullscr=fullscr
     )
+win.setMouseVisible(False)
 
 # Sinusoidal control version.
-freq_one = 12
-freq_two = 20
+freq_one = 0.5
+freq_two = 1.5
+# Colors of the rectangles.
+color_one = 'red'
+color_two = 'green'
+# Positions of the rectanges.
+pos_one = (-7, 0)
+pos_two = (7, 0)
 
 
 start = core.getTime()
 cnt = 0
-while cnt<150:
+while cnt<600:
     second = core.getTime() - start
 
     sin_val_one = 0.5+0.5*np.sin(2 * np.pi * second * float(freq_one))
-    print(sin_val_one)
     sin_val_two = 0.5+0.5*np.sin(2 * np.pi * second * float(freq_two))
     
     rect_one = visual.Rect(
         win=win,
-        fillColor='red',
-        lineWidth=0.0, 
+        fillColor=color_one,
+        lineColor=color_one, 
         size=20,
-        pos=(-7, 0),
+        pos=pos_one,
         opacity=sin_val_one
         )
     rect_two = visual.Rect(
         win=win,
-        fillColor='blue',
-        lineWidth=0.0, 
+        fillColor=color_two,
+        lineColor=color_two, 
         size=20,
-        pos=(7, 0),
+        pos=pos_two,
         opacity=sin_val_two
         )
 
-    if sin_val_one >= 0:
-        rect_one.draw()
-    if sin_val_two >= 0:
-        rect_two.draw()
+    rect_one.draw()
+    rect_two.draw()
     win.flip()
     cnt += 1
 
