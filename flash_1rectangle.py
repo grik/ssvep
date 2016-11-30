a78f4ab2aadb76ce48a764ca61f4be83f485b7b3
mikbuch <mikolaj.buchwald@gmail.com>
2016-11-30 01:44:15 +0100

    one rectangle, better example

--- flash_1rectangle.py
+++ flash_1rectangle.py
@@ -10,34 +10,45 @@ import numpy as np
 #
 
 # Create a window.
+# For configuring and debugging the code turn off full screen.
+fullscr = False
 win = visual.Window(
     [1200,1000],
     monitor="testMonitor",
-    units="deg"
+    units="deg",
+    fullscr=fullscr
     )
+win.setMouseVisible(False)
 
 # Sinusoidal control frequency.
-freq = 12
+freq = 1.5
+# Color of the rectangle.
+color = '#606a79'
+# Position of the rectange, default 0,0 (middle of the screen).
+pos = (0, 0)
 
 start = core.getTime()
 cnt = 0
-while cnt<150:
+while cnt<300:
     second = core.getTime() - start
     sin_val = 0.5+0.5*np.sin(
         2 * np.pi * second * float(freq)
         )
+    # If you remove or comment this print, it sould work faster
+    print('sec: %.4f; sin: %.4f' % (second, sin_val))
     
     rect = visual.Rect(
         win=win,
-        lineWidth=0.0, 
-        fillColor='red',
+        lineColor=color, 
+        fillColor=color,
         size=20,
-        opacity=sin_val
+        opacity=sin_val,
+        pos=pos
         )
 
-    if sin_val >= 0:
-        rect.draw() 
+    rect.draw() 
     win.flip()
+
     cnt += 1
 
 win.close()
